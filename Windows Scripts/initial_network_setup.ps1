6bf99e2abd5ee3d72b5f92f8bf9ff884e753e534
Dylan-Busch <97697430+Dylan-Busch@users.noreply.github.com>
2025-12-17 20:46:23 -0600

    added files

---
+++ Windows Scripts/initial_network_setup.ps1
@@ -0,0 +1,25 @@
+﻿# Configure a Windows System with static IP address, gateway, and DNS
+
+# Ensure PowerShell is ran as administrator
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdministrator = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ($isAdministrator -eq $false) {
+    Write-Host 'Please run PowerShell as Administrator' -foregroundcolor Red
+    exit
+}
+
+param(
+    [string]$ipaddress = '192.168.1.11', # CHANGE TO DESIRED HOST IP ADRESS
+    [string]$subnetmask = '24',
+    [string]$defaultgateway = '192.168.1.1', # CHANGE TO ENVIRONMENT FORWARDING ROUTER
+    [string]$dnsserver = '192.168.1.1',
+    [string]$interfacealias = 'Ethernet0'
+)
+
+# Set IP Address & Default Gateway
+New-NetIPAddress -InterfaceAlias $interfacealias -IPAddress $ipaddress -PrefixLength $subnetmask -DefaultGateway $defaultgateway
+Write-Host 'Network address configured' -ForegroundColor Cyan
+
+# Set DNS Address
+Set-DnsClientServerAddress -InterfaceAlias $interfacealias -ServerAddresses $dnsserver
+Write-Host 'DNS address configured' -ForegroundColor Cyan
---
+++ Windows Scripts/ssh_ping_setup.ps1
@@ -0,0 +1,30 @@
+﻿# Install OpenSSH if not already installed and allow ping through firewall
+
+# Ensure PowerShell is ran as administrator
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdministrator = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ($isAdministrator -eq $false) {
+    Write-Host 'Please run PowerShell as Administrator' -foregroundcolor Red
+    exit
+}
+
+# Install OpenSSH
+$opensshfeature = Get-WindowsCapability -Online | Where-Object Name -Like 'OpenSSH.Server*'
+if ($opensshfeature -ne 'Installed') {
+ Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'
+ Write-Host 'OpenSSH Server successfully installed' -ForegroundColor Cyan
+} else {
+    Write-Host 'OpenSSH Server is already installed' -ForegroundColor Cyan
+}
+
+# Start OpenSSH automatically
+Start-Service -Name sshd
+Set-Service -Name sshd -StartupType Automatic
+
+
+# Enable ICMP Firewall Rule
+Enable-NetFirewallRule -displayName 'File and Printer Sharing (Echo Request - ICMPv4-In)'
+Write-Host 'ICMP ping firewall rule enabled' -ForegroundColor Cyan
+
+# Set default shell to powershell
+New-ItemProperty -Path 'HKLM:\SOFTWARE\OpenSSH'-Name DefaultShell -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -PropertyType String -Force
